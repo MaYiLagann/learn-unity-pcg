27e807107b5221ad516e2c533af29c60ee15c0f5
MaYiLagann <lhwshy@naver.com>
2022-09-17 20:39:51 +0900

    Create script DungeonManager

---
+++ Assets/Scripts/DungeonManager.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Random = UnityEngine.Random;
+
+public enum TileType
+{
+    Empty,
+    Essential,
+    Random,
+}
+
+public class DungeonManager : MonoBehaviour
+{
+
+}
